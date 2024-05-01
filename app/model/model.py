bf83e7d2a182cf49c715a1ea321ef903c607285a
Leon Lakhani <leonl@berkeley.edu>
2024-04-30 19:39:17 -0700

    sdfd

--- app/model/model.py
+++ app/model/model.py
@@ -7,7 +7,7 @@ __version__ = "0.1.0"
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 
-with open(f"{BASE_DIR}/trained_pipeline-{__version__}.pkl", "rb") as f:
+with open(f"{BASE_DIR}/simple_langauge_classifier-{__version__}.pkl", "rb") as f:
     model = pickle.load(f)
 
 
